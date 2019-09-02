8cfd42fc85f747d096bd731799d25aa87cd818d4
Cheng Ye <chengye615@gmail.com>
2019-09-01 22:16:57 -0400

    Added necessary files

---
+++ spell_checker.py
@@ -0,0 +1,28 @@
+"""
+File name: spell_checker.py
+Version: python 3.7
+Author: Cheng Ye
+
+Spell checks and corrects typos
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass()
+class SpellChecker:
+
+    def __init__( self ):
+        dictionary = None
+        adjacent = None
+
+
+    def getDictionary( self ):
+
+
+    def run( self ):
+
+
+
+
+def main():
